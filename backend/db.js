44c0cda1a813c06da0305cab2818b8baa8a6e0df
shivamg5501 <shivamg998764@gmail.com>
2024-11-26 18:01:06 +0530

    main updates

---
+++ backend/db.js
@@ -0,0 +1,56 @@
+// backend/db.js
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://shivamg998764:P4m4ZM6EAKeu2vSg@cluster0.3dmlh.mongodb.net/main"
+);
+// Create a Schema for Users
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+});
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+const Account = mongoose.model('Account', accountSchema);
+
+// Create a model from the schema
+const User = mongoose.model("User", userSchema);
+
+module.exports = {
+  User,
+  Account
+};
